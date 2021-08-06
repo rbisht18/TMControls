c1fc87c31754e7a6b956e7bb1f8e9fafe2b975bd
Ranjeet <rb00612076@techmahindra.com>
2021-08-06 13:22:52 +0530

    Make underlayColor dynamic

--- src/reusableComponents/PrimaryButton.js
+++ src/reusableComponents/PrimaryButton.js
@@ -7,7 +7,7 @@ const width = Dimensions.get('window').width;
 const PrimaryButton = (props) => {
     return (
         <View style={props.buttonType == 'Outline' ? styles.outlineContainer: styles.container}>
-            <TouchableHighlight onPress={() => props.onPress()} underlayColor="#141414" 
+            <TouchableHighlight onPress={() => props.onPress()} underlayColor={props.underlayColor} 
             style={[props.buttonType == 'Outline' ? styles.outlineButtonContainer : styles.buttonContainer,props.overideTouchStyle]} >
                 <Text style={[styles.text, props.overideTextStyle]}>{props.name}</Text>
             </TouchableHighlight>
--- src/reusableComponents/SecondaryButton.js
+++ src/reusableComponents/SecondaryButton.js
@@ -10,7 +10,7 @@ const width = Dimensions.get('window').width;
 const SecondaryButton = (props) => {
     return (
         <View style={props.buttonType == 'Outline' ? styles.outlineContainer: styles.container}>
-            <TouchableHighlight onPress={() => props.onPress()} underlayColor="#D7D7D7" 
+            <TouchableHighlight onPress={() => props.onPress()} underlayColor={props.underlayColor} 
             style={[props.buttonType == 'Outline' ? styles.outlineButtonContainer : styles.buttonContainer,props.overideTouchStyle]} >
                 <Text style={[styles.text, props.overideTextStyle]}>{props.name}</Text>
             </TouchableHighlight>
